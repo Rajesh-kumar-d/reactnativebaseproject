e838bdbe60748bc1a15dad76d606842247799b1a
Rajesh-kumar-d <85670490+Rajesh-kumar-d@users.noreply.github.com>
2021-06-10 20:40:52 +0530

    initialPush
    scratch project

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import { StyleSheet,Text,View } from 'react-native';
+
+const App: () => Node = () => {
+  return (
+        <View>
+           <Text>
+              Saravananaaqdddddddddddd
+asd
+              </Text>
+        </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
